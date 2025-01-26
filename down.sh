cdf479e3c97856f635a75bc98ee4e97534d758e5
Bondarenko Nikita <bondar3nko.nikit@yandex.ru>
2025-01-26 18:04:02 +0000

    My final project

--- down.sh
+++
@@ -1,4 +0,0 @@
-export LOCAL_IP=$(hostname -I | awk '{print $1}')
-export EXTERNAL_IP=$(curl -s ifconfig.me)
-docker-compose -f app.compose.yml -p rbmdkrfinalapp down
-docker-compose -f efk.compose.yml -p rbmdkrfinalefk down
